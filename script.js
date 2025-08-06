3ea21c5c006459bd73ac9edae333f662375eef7f
Monalize M <150396638+mlize08@users.noreply.github.com>
2025-08-06 10:46:48 -0300

    s

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ function inserirNome() {
 
 inserirNome();
 
-let linguagens = ["Javascript", "Python", "C"];
-const item = document.querySelector("#lista");
-item.textContent = linguagens[3];
+let linguagens = ["Javascript", "Python", "C", "PHP"];
+const lista= document.querySelector("#lista");
+
+for (let linguagem of linguagens) {
+  lista.innerHTML += `<li>${linguagem}</li>`;
+}
+
+
+
+
+
 
 let aluno1 = {
   nome: "Gabriel",
@@ -18,3 +26,23 @@ let aluno1 = {
 }
 
 console.log(aluno1.nome);
+console.log(aluno1.idade);
+console.log(aluno1.anoLetivo);
+console.log(aluno1.materiasFavoritas);
+console.log(aluno1.materiasFavoritas[2]);
+
+const objeto = document.querySelector("#aluno");
+objeto.textContent = aluno1.nome;
+objeto.textContent = aluno1.idade;
+objeto.textContent = aluno1.materiasFavoritas;
+objeto.textContent = aluno1.materiasFavoritas[0];
+
+
+
+
+//template String
+console.log(`Nome: ${aluno1.nome}`);
+console.log(`Idade: ${aluno1.idade}`);
+console.log(`Ano Letivo: ${aluno1.anoLetivo}`);
+console.log(`Materias Favoritas: ${aluno1.materiasFavoritas}`);
+console.log(`Materias Favorita: ${aluno1.materiasFavoritas[2]}`);
